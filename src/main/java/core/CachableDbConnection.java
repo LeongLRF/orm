0daf112a77214cf1c3f2b769ef91caed83a7a43d
Leong <980159737@qq.com>
2019-11-26 18:14:01 +0800

    redis缓存

--- src/main/java/core/CachableDbConnection.java
+++ src/main/java/core/CachableDbConnection.java
@@ -1,6 +1,5 @@
 package core;
 
-import annotation.Table;
 import com.alibaba.fastjson.JSON;
 import config.Configuration;
 import redis.clients.jedis.Jedis;
@@ -10,11 +9,9 @@ import util.EntityUtil;
 import javax.sql.DataSource;
 import java.io.Serializable;
 import java.sql.SQLException;
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 public class CachableDbConnection extends DbConnection {
@@ -28,36 +25,36 @@ public class CachableDbConnection extends DbConnection {
 
     @Override
     public <T> T getById(Class<T> cls, Serializable id) {
-        return super.getById(cls, id);
+        return getCache(cls, id);
     }
 
     @Override
     public <T> List<T> getByIds(Class<T> cls, List<Object> ids) {
-        return super.getByIds(cls, ids);
+        return getCaches(cls, ids);
     }
 
     @Override
     public <T> int insert(T entity) {
+        setValue(entity);
         return super.insert(entity);
     }
 
     @Override
     public <T> int insert(List<T> entities) {
+        setValue(entities);
         return super.insert(entities);
     }
 
-    @Override
-    public void openTransaction(Supplier<?> f) {
-        super.openTransaction(f);
-    }
 
     @Override
     public <T> void updateById(Class<T> cls, Serializable id, Consumer<T> updates) {
+        del(cls,id);
         super.updateById(cls, id, updates);
     }
 
     @Override
     public <T> int update(T entity) {
+        del(entity);
         return super.update(entity);
     }
 
@@ -113,8 +110,37 @@ public class CachableDbConnection extends DbConnection {
         return tableInfo.getTableName();
     }
 
-    private void setValue(String value, String key,int seconds) {
-        op(j -> j.setex(key,seconds,value));
+    private void setValue(String value, String key, int seconds) {
+        op(j -> j.setex(key, seconds, value));
+    }
+
+    private <T> void setValue(T entity) {
+        Class<?> cls = entity.getClass();
+        TableInfo tableInfo = EntityUtil.getTableInfo(cls);
+        String key = redisKey(prefix(tableInfo), EntityUtil.getId(entity));
+        setValue(JSON.toJSONString(entity), key, tableInfo.getExpireTime());
+    }
+
+    private <T> void setValue(List<T> entities) {
+        entities.forEach(this::setValue);
+    }
+    private <T> void del(T entity){
+        Class<?> cls = entity.getClass();
+        Object id = EntityUtil.getId(entity);
+        del(cls, (Serializable) id);
+    }
+
+    private <T> void del(Class<T> cls, Serializable id) {
+        TableInfo tableInfo = EntityUtil.getTableInfo(cls);
+        String key = redisKey(prefix(tableInfo), id);
+        del(key);
+    }
+    private <T> void del(List<T> entities){
+
+    }
+
+    private void del(String key) {
+        op(j -> j.del(key));
     }
 
     private <T> T op(Function<Jedis, T> op) {
