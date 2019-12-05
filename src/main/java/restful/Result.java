13014fdb15a1cbf90e55d18d989cb72a2244d055
Leong <980159737@qq.com>
2019-12-05 09:45:34 +0800

    debug

---
+++ src/main/java/restful/Result.java
@@ -0,0 +1,113 @@
+package restful;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+/**
+ * @author Leong
+ * RestFul API
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+public class Result<T> {
+
+    public boolean success;
+    public T data;
+    public String code;
+    public String message;
+
+    public static <T> Result<T> ok(String message, T data) {
+        return new Result<>(true, data, "", message);
+    }
+
+    public static <T> Result<T> ok() {
+        return ok("ok", null);
+    }
+
+    public static <T> Result<T> ok(T data) {
+        return ok("ok", data);
+    }
+
+    public static <T> Result<T> error(String message) {
+        return new Result<>(false, null, "9999", message);
+    }
+
+    public static <T> Result<T> error(String message, String code) {
+        return new Result<>(false, null, code, message);
+    }
+
+    public static <T> Result<T> maybe(T data, String errorMsg) {
+        if (data == null) {
+            return error(errorMsg);
+        } else {
+            return ok(data);
+        }
+    }
+
+    public static <T> Result<T> maybe(T data) {
+        return maybe(data, "数据不存在");
+    }
+
+    public static <T> Result<T> maybe(Optional<T> data, String errorMsg) {
+        return data.map(Result::ok).orElseGet(() -> error(errorMsg));
+    }
+
+    public <R> Result<R> andThen(Function<T, Result<R>> f) {
+        if (success) {
+            return f.apply(data);
+        } else {
+            return error(message, code);
+        }
+    }
+
+    public <R> Result<R> andThenMaybe(Function<T, R> f) {
+        return andThen(p -> maybe(f.apply(p)));
+    }
+
+    public Result<T> andThenCheck(Predicate<T> f, String errorMsg) {
+        if (success) {
+            if (f.test(this.data)) {
+                return ok(data);
+            } else {
+                return error(errorMsg);
+            }
+        } else {
+            return this;
+        }
+    }
+
+    public Result<T> andThenCheck(boolean test, String errorMsg) {
+        if (success) {
+            if (test) {
+                return ok(data);
+            } else {
+                return error(errorMsg);
+            }
+        } else {
+            return this;
+        }
+    }
+
+    public <R> Result<T> mapSelf(Function<T, R> f) {
+        if (success) {
+            f.apply(data);
+            return this;
+        } else {
+            return error(message, code);
+        }
+    }
+
+    public Result<T> accept(Consumer<T> f) {
+        if (success) {
+            f.accept(data);
+            return this;
+        } else {
+            return error(message, code);
+        }
+
+    }
+}
---
+++ src/main/java/util/Preloader.java
@@ -0,0 +1,46 @@
+package util;
+
+import core.inerface.IDbConnection;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * @author Leong
+ * @date 2019/9/25 16:44
+ */
+public class Preloader<T> {
+    private IDbConnection db;
+    private List<T> data;
+
+    public Preloader(IDbConnection db, List<T> list) {
+        this.db = db;
+        this.data = list;
+    }
+
+
+    public <R> Preloader<R> preload_many(Class<R> cls,Function<T,Object> getId,Function<R,Object> f, String realKey, BiConsumer<T, List<R>> setter) {
+        List<Object> ids = data.stream().map(getId).distinct().collect(Collectors.toList());
+        List<R> linkedEntity;
+        linkedEntity = db.form(cls).in(realKey, ids).toList();
+        Map<Object, List<R>> map = linkedEntity.stream().collect(Collectors.groupingBy(f));
+        for (T entity : data) {
+            List<R> r = map.get(getId.apply(entity));
+            setter.accept(entity, r);
+        }
+        return new Preloader<>(db,linkedEntity);
+    }
+
+    public <R> void preload_one(Class<R> cls,Function<T, Object> getId,Function<R,Object> f, String realKey, BiConsumer<T, R> setter) {
+        preload_many(cls,getId,f, realKey,(t, list) -> {
+            if (list!=null&&!list.isEmpty()) {
+                R r = list.get(0);
+                setter.accept(t, r);
+            }
+        });
+    }
+}
