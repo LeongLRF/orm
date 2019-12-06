880ca6ca9811dd1475acc60a718bc56bb4a9a8d9
Leong <980159737@qq.com>
2019-12-06 08:43:41 +0800

    debug

--- src/main/java/util/Preloader.java
+++ src/main/java/util/Preloader.java
@@ -23,21 +23,20 @@ public class Preloader<T> {
     }
 
 
-    public <R> Preloader<R> preload_many(Class<R> cls,Function<T,Object> getId,Function<R,Object> f, String realKey, BiConsumer<T, List<R>> setter) {
+    public <R> Preloader<R> preloadMany(Class<R> cls, Function<T, Object> getId, Function<R, Object> f, String realKey, BiConsumer<T, List<R>> setter) {
         List<Object> ids = data.stream().map(getId).distinct().collect(Collectors.toList());
-        List<R> linkedEntity;
-        linkedEntity = db.form(cls).in(realKey, ids).toList();
+        List<R> linkedEntity = db.form(cls).in(realKey, ids).toList();
         Map<Object, List<R>> map = linkedEntity.stream().collect(Collectors.groupingBy(f));
         for (T entity : data) {
             List<R> r = map.get(getId.apply(entity));
             setter.accept(entity, r);
         }
-        return new Preloader<>(db,linkedEntity);
+        return new Preloader<>(db, linkedEntity);
     }
 
-    public <R> void preload_one(Class<R> cls,Function<T, Object> getId,Function<R,Object> f, String realKey, BiConsumer<T, R> setter) {
-        preload_many(cls,getId,f, realKey,(t, list) -> {
-            if (list!=null&&!list.isEmpty()) {
+    public <R> Preloader<R> preloadOne(Class<R> cls, Function<T, Object> getId, Function<R, Object> f, String realKey, BiConsumer<T, R> setter) {
+        return preloadMany(cls, getId, f, realKey, (t, list) -> {
+            if (list != null && !list.isEmpty()) {
                 R r = list.get(0);
                 setter.accept(t, r);
             }
